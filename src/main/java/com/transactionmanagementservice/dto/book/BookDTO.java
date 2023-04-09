4859e33a6492861c73f090508fedbeabaee0937b
georgrybski <rybskigeorg@gmail.com>
2023-04-08 23:34:48 -0300

    Added BookDTO

---
+++ src/main/java/com/transactionmanagementservice/dto/book/BookDTO.java
@@ -0,0 +1,13 @@
+package com.transactionmanagementservice.dto.book;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class BookDTO {
+    private Long id;
+    private String title;
+}
