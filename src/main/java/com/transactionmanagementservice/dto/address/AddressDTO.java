9263ae4653428f04130d44f1b5550f7074af46c2
georgrybski <rybskigeorg@gmail.com>
2023-04-09 00:46:54 -0300

    Added AddressDTO

---
+++ src/main/java/com/transactionmanagementservice/dto/address/AddressDTO.java
@@ -0,0 +1,17 @@
+package com.transactionmanagementservice.dto.address;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddressDTO {
+    private Long id;
+    private String fullName;
+    private String phoneNumber;
+    private String street;
+    private String number;
+    private String city;
+    private String country;
+    private String zipCodeOrPostalCodeOrCEP;
+}
